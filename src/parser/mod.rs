88d97fbf31731800f0d1b7c87b98e2df8a301eac
Hugo <57953645+hugo-k-m@users.noreply.github.com>
2022-06-03 17:24:50 +0200

    Added macro for configuring the parser's attributes

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,16 +1,41 @@
+use lazy_static::lazy_static;
+use std::collections::HashMap;
 use structopt::StructOpt;
 
+lazy_static! {
+    static ref PARSER_OPTIONS: HashMap<&'static str, &'static str> = {
+        let mut hash_map = HashMap::new();
+
+        hash_map.insert(
+            "parser about",
+            "Media tool for clipping videos, extracting\
+            frames, and other features.",
+        );
+
+        hash_map
+    };
+}
+
+macro_rules! attribute_parameter {
+    ($option:expr) => {
+        PARSER_OPTIONS.get($option).unwrap().to_owned()
+    };
+}
+
 #[derive(Debug, StructOpt)]
+#[structopt(about = attribute_parameter!("parser about"))]
 pub enum Parser {
     /// Clip the video
     Clip {
         /// Specifies the input file
+        #[structopt(short, long)]
         infile: String,
     },
 
     /// Extract a video's frames
     Frames {
         /// Specifies the input file
+        #[structopt(short, long)]
         infile: String,
     },
 }
