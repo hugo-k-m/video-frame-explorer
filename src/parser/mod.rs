dcee7d1a565a1c028c0317c95da54956e1406ca3
Hugo <57953645+hugo-k-m@users.noreply.github.com>
2022-06-09 21:13:14 +0200

    Refactored subcommands

--- src/parser/mod.rs
+++ src/parser/mod.rs
@@ -1,22 +1,53 @@
 mod parser_attribute_parameters;
 
-use super::parser::parser_attribute_parameters::{attribute_parameter, PARSER_OPTIONS};
+use super::parser::parser_attribute_parameters::*;
+use std::path::PathBuf;
 use structopt::StructOpt;
 
 #[derive(Debug, StructOpt)]
-#[structopt(about = attribute_parameter!("parser about"))]
-pub enum Parser {
+enum Command {
     /// Clip the video
     Clip {
-        /// Specifies the input file
+        /// specifies the input file
+        #[structopt(short, long, parse(from_os_str))]
+        infile: PathBuf,
+
+        /// set the start time offset
+        ss: Option<String>,
+
+        /// record or transcode "duration" seconds of audio/video
+        t: String,
+
+        /// record or transcode stop time
+        to: String,
+    },
+
+    /// Virtual concatenation script demuxer.
+    ConcatDemuxer {
+        /// specifies the input list
         #[structopt(short, long)]
         infile: String,
+
+        /// select an encoder or a decoder
+        #[structopt(short, long)]
+        codec: String,
     },
 
     /// Extract a video's frames
     Frames {
-        /// Specifies the input file
+        /// specifies the input file
         #[structopt(short, long)]
         infile: String,
+
+        /// video sync method
+        #[structopt(short, long)]
+        vsync: Option<String>,
     },
+}
+
+#[derive(StructOpt)]
+#[structopt(about = attribute_parameter!("parser about"))]
+pub struct Parser {
+    #[structopt(subcommand)]
+    cmd: Command,
 }
